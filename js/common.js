689cdfb37c6e8a5c6da79beb9e2f1d6e4ad04b47
宮崎 雄策 <toumin.m7@gmail.com>
2015-12-05 16:15:31 +0900

    modify common.js

--- js/common.js
+++ js/common.js
@@ -1,42 +1,50 @@
 /**
- * グローバルナビをPC/スマホで切り替える。
- * jQueryで要素を非表示にしているため、CSSのブレークポイントでは再表示することができない。
+ * グローバルナビの表示をPC, Mobileで切り替える
+ * 要素をjQueryで非表示にしているため、CSSでは再表示できない。
  * そのため、下記の関数は必要。
  */
 function globalNavi () {
-  if (window.innerWidth < 768) {
-    $('.globalnavi').hide(); // スマホ向け
+  // window.innerWidth ではなく、各ブレークポイントでの要素の表示状態で判断する。
+  // 参照: http://b.hatena.ne.jp/entry/245120894/comment/datemakio
+  if ($('.toggle-menu').is(':visible')) {
+    $('.globalnavi, .globalnavi > *').hide(); // スマホ向け
   } else {
-    $('.globalnavi').show(); // PC向け
+    $('.globalnavi, .globalnavi > *').show(); // PC向け
   }
 }
+
 /**
  * ページ読込み後に行う処理
  */
 $(function () {
-  /**
-   * 事前実行
-   */
+  // 事前実行
   globalNavi();
-  /**
-   * レスポンシブ対応
-   */
+  // レスポンシブ対応
   $(window).on('resize', function() {
     globalNavi();
   });
+
   /**
-   * for Mobile: ドロワーメニュー
+   * グローバルナビの表示切替
+   * 単純なslideUp(), slideDown()ではアニメーションしない可能性がある。
+   * そのため、対象の子要素にslideUp()させるなど、複雑な指定をしている。
+   * 参照: 
    */
-  $(document).on('click', function () {
-    if (window.innerWidth < 768) {
-      $(this).toggleClass('toggle-menu-open');
-      $('.toggle-menu').removeClass('toggle-menu-open');
-      $('.globalnavi').slideUp('fast');
+  $(document).on('click', function (ev) {
+    if ($('.toggle-menu').is(':visible')) {
+      ev.stopPropagation();
+      $('.globalnavi > *').slideUp('fast', function () {
+        $(this).parent().hide();
+      });
     }
   });
   $('.toggle-menu').on('click', function (ev) {
-    ev.stopPropagation();
-    $(this).toggleClass('toggle-menu-open');
-    $('.globalnavi').slideToggle('fast');
+    if ($('.globalnavi').is(':hidden')) {
+      ev.stopPropagation();
+      $('.globalnavi > *')
+        .hide()
+        .parent().show().end()
+        .slideDown('fast');
+    }
   });
 });
